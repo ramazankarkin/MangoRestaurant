598362323855738f6809b100b54304ced599ded6
ramazan karkin <ramazan.krkn@gmail.com>
2022-08-11 17:58:12 +0300

    Duende Identity Server added for authorization and authentication

---
+++ Mango.Services.Identity/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Services.Identity
+{
+    public static class SD
+    {
+        public const string Admin = "Admin";
+        public const string Customer = "Customer";
+
+    }
+}
